1b212c00ad3c0456ffb6a1f05647f59aad5c86ed
Alishehzada910 <alishehzada910@gmail.com>
2025-09-07 12:40:22 +0500

    Create Shehzada.js

---
+++ Priyansh/commands/Shehzada.js
@@ -0,0 +1,23 @@
+module.exports.config = {
+  name: "Shehzada",
+  version: "1.0.0",
+  hasPermssion: 0,
+  credits: "JULMI JAAT",
+  description: "Shehzada",
+  commandCategory: "one answer",
+  usages: "Jani",
+  cooldowns: 2,
+  dependencies: {
+    "request":"",
+    "fs-extra":"janu",
+    "axios":""
+  }
+    
+};
+
+module.exports.run = async({api,event,args,Users,Threads,Currencies}) => {
+const axios = global.nodemodule["axios"];
+const request = global.nodemodule["request"];
+const fs = global.nodemodule["fs-extra"];
+    var link = ["Owner Shaded SINGLE ha Us Sy Rabta krohttps://www.facebook.com/share/19pXU3PagG/kro"]
+    
