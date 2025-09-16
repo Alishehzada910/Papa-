d86da5026ad20eddf99f009dfffa7998eddc6158
Alishehzada910 <alishehzada910@gmail.com>
2025-09-16 12:55:14 +0500

    Update admin_det.js

--- Priyansh/commands/admin_det.js
+++ Priyansh/commands/admin_det.js
@@ -16,7 +16,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
     var msg = {
         body: "🫅 𝐎𝐖𝐍𝐄𝐑 𝐏𝐑𝐎𝐅𝐈𝐋𝐄 🫅",
         attachment: 
-fs.createReadStream(__dirname + `https://ibb.co/GvYwf8wt`)
+fs.createReadStream(__dirname + `https://i.postimg.cc/pLRg4f8C/20250912-162252.jpg`)
       }
       api.sendMessage(msg, threadID, messageID);
     api.setMessageReaction("🫅", event.messageID, (err) => {}, true)
