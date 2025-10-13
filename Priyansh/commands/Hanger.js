accb4f6acd4113980740f8b7333bde0df3bd7d6f
Alishehzada910 <alishehzada910@gmail.com>
2025-10-14 00:11:24 +0500

    Update Hanger.js

--- Priyansh/commands/Hanger.js
+++ Priyansh/commands/Hanger.js
@@ -1,11 +1,11 @@
 module.exports.config = {
-    name: "fyt",
+    name: "hanger",
     version: "1.0.0",
     hasPermssion: 2,
     credits: "𝐏𝐫𝐢𝐲𝐚𝐧𝐬𝐡 𝐑𝐚𝐣𝐩𝐮𝐭",
     description: "War In Chatbox",
     commandCategory: "wargroup",
-    usages: "[hanger]",
+    usages: "[ummmah]",
     cooldowns: 7,
     dependencies: {
         "fs-extra": "",
